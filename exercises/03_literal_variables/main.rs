bd3feb4dc0fda6076d91d37ae42ec74376c887c1
Abner Zheng <abnerzheng@gmail.com>
2024-03-31 15:04:54 +0800

    finish exercise 3

--- exercises/03_literal_variables/main.rs
+++ exercises/03_literal_variables/main.rs
@@ -4,7 +4,10 @@ fn print_result(num: i32) {
 }
 ////////// DO NOT CHANGE ABOVE HERE /////////
 
-// TODO: create `math!()` macro.
+macro_rules! math {
+    ($a: literal plus $b: literal) => {$a + $b};
+    (square $a: literal) => {$a * $a};
+}
 
 ////////// DO NOT CHANGE BELOW HERE /////////
 
