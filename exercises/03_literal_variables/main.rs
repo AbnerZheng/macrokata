27549b25ab6a92d2f6fb3c05a25bd02c10d65d1a
Abner Zheng <abnerzheng@gmail.com>
2024-03-31 15:20:03 +0800

    finish exercise4

--- exercises/03_literal_variables/main.rs
+++ exercises/03_literal_variables/main.rs
@@ -12,6 +12,6 @@ macro_rules! math {
 ////////// DO NOT CHANGE BELOW HERE /////////
 
 fn main() {
-    print_result(math!(3 plus 5));
+    print_result(2 * math!(3 plus 5));
     print_result(math!(square 2));
 }
