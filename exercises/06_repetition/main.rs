f79d8ee469a0cbe3f16815b4f901f248a908a767
Abner Zheng <abnerzheng@gmail.com>
2024-03-31 15:55:48 +0800

    finish exercise 6

--- exercises/06_repetition/main.rs
+++ exercises/06_repetition/main.rs
@@ -4,7 +4,11 @@ fn print_success() {
 }
 ////////// DO NOT CHANGE ABOVE HERE /////////
 
-// TODO: create `if_any!()` macro.
+macro_rules! if_any {
+    ($a: expr $(, $e: expr)+ ; $b: block) => {
+        if $a $(|| $e)+ $b
+    };
+}
 
 ////////// DO NOT CHANGE BELOW HERE /////////
 
